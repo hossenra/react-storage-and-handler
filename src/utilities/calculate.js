ec97d36dc92cc930b59d5d2d6bd0e64a5ed901bf
RAZIBUL HOSSEN <razibulhossen@MacBook-Air-3.local>
2022-09-30 11:51:33 +0200

    first commit

---
+++ src/utilities/calculate.js
@@ -0,0 +1,30 @@
+const add = (first, second) =>{
+ return first + second;
+}
+
+const multiply = (first, second) =>{
+ return first * second;
+}
+
+// const numbers = [20, 24, 235, 68, 45];
+// const sumReducer = (previous, current) => previous + current;
+// const total = numbers.reduce(sumReducer, 0)
+
+// const items = [
+//  {id: 1, name: 'alta', price: 100},
+//  {id: 2, name: 'alta', price: 100},
+//  {id: 3, name: 'alta', price: 100},
+//  {id: 4, name: 'alta', price: 100},
+//  {id: 5, name: 'alta', price: 100}
+// ]
+
+// const itemSumReducer = (previous, current) => previous + current.price;
+// const itemTotal = items.reduce(itemSumReducer ,0);
+
+const getTotalPrice = products =>{
+ const reducer = (previous, current) => previous + current.price;
+ const total = products.reduce(reducer, 0);
+ return total;
+}
+
+export{add, multiply, getTotalPrice as getTotal};
